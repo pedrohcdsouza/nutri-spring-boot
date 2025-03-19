a32d4f1627c9d222441bca23376c3366656380fa
Pedro Henrique Cardoso de Souza <pedrohcsousa8@hotmail.com>
2025-03-19 20:06:16 -0300

    Starting project

---
+++ src/main/java/com/lunae/nutrivivi/NutriviviApplication.java
@@ -0,0 +1,13 @@
+package com.lunae.nutrivivi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutriviviApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NutriviviApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/lunae/nutrivivi/auth/model/Role.java
@@ -0,0 +1,6 @@
+package com.lunae.nutrivivi.auth.model;
+
+public enum Role {
+    DEFAULT,
+    ADMIN
+}
