fd3ce9410a0442162ae21e00e68c4efb88811411
Pedro Henrique Cardoso de Souza <pedrohcsousa8@hotmail.com>
2025-03-22 12:20:02 -0300

    Reseting project

--- src/main/java/com/lunae/nutrivivi/auth/model/Role.java
+++
@@ -1,6 +0,0 @@
-package com.lunae.nutrivivi.auth.model;
-
-public enum Role {
-    DEFAULT,
-    ADMIN
-}
--- src/main/java/com/lunae/nutrivivi/auth/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.lunae.nutrivivi.auth.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.lunae.nutrivivi.auth.model.User;
-import java.util.Optional;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByUsername(String username);
-}
