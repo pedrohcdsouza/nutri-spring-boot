ce3f40f82191a1615e98e610637a3d70426527cc
Pedro Henrique Cardoso de Souza <pedrohcsousa8@hotmail.com>
2025-03-22 13:17:02 -0300

    Initializing project

---
+++ web-core/src/main/java/br/com/nutrivivi/web_core/WebCoreApplication.java
@@ -0,0 +1,13 @@
+package br.com.nutrivivi.web_core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebCoreApplication.class, args);
+	}
+
+}
---
+++ web-core/src/test/java/br/com/nutrivivi/web_core/WebCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.nutrivivi.web_core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
